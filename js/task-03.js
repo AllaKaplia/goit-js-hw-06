d1f937316f8c460fb15afdea91c005ae2cdf335d
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-03-30 01:08:50 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -16,4 +16,4 @@ const images = [
 
 const galleryEl = document.querySelector('.gallery');
 const makeListOfImages = images.map(image => `<li class="item-image"><img src="${image.url}" alt="${image.alt}" ></li>`);
-galleryEl.insertAdjacentHTML('beforeend', makeListOfImages);
+galleryEl.insertAdjacentHTML('beforeend', makeListOfImages.join(''));
